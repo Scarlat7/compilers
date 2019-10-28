26a6b7194f97c7ba311e735ea0f98ea7ab208fa1
Scarlat7 <nataliarampon@hotmail.com>
2019-10-27 21:35:51 -0400

    Addition of AST test function

---
+++ ast_test.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+#############################################################
+#						FOR E3 ONLY							#
+#					Execute with Python3					#
+#############################################################
+
+VALGRIND=false
+CONVERT_SCRIPT=convert_csv_dot.py
+TCNO_SCRIPT=ast_test.py
+
+while [ "$1" != "" ]; do
+	case $1 in
+		-m | --valgrind )		VALGRIND=true
+								;;	
+		* )				exit 1
+	esac
+	shift
+done
+
+rm test_result.txt
+echo "test, tcno, valgrind" >> test_result.txt
+
+TEST_DIR="test3/"
+
+for test in `ls $TEST_DIR -I '*.*'` `ls $TEST_DIR*.ptg`
+do
+	printf "$test," >> test_result.txt
+	if [ "$VALGRIND" = true ]; then
+		# Executes with valgrind		
+		rm -f valgrind.txt
+		valgrind ./etapa3/etapa3 < $TEST_DIR$test 2>&1 | grep -A5 LEAK\ SUMMARY > valgrind.txt
+		VGRESULT=$(cat valgrind.txt | cut -d":" -f2| awk '{print $1}' | tr '\n' ' ' | sed "s/,//g")
+		if [[ $VGRESULT = "" ]]; then
+		    VGRESULT="no leaks are possible"
+		    VG=0
+		else
+		    VG=$(echo $VGRESULT | tr ' ' '+' | bc)
+		fi
+	else
+		# Executes without valgrind
+		cat $TEST_DIR$test | ./etapa3/etapa3
+	fi
+
+	# At this point we have the output at ./e3.csv
+	DOT_REF=test3/$test.ref.dot
+	DOT_GEN=test3/$test.gen.dot
+
+	# Turn the output into a .dot file
+	python3 $CONVERT_SCRIPT e3.csv >	$DOT_GEN
+
+	TCNO_RESULT=$(python3 $TCNO_SCRIPT $(pwd)/$DOT_GEN $(pwd)/$DOT_REF)
+	TCNOCODE=$?
+
+	printf "$TCNOCODE," >> test_result.txt
+	echo "$VG" >> test_result.txt
+done
